67c678947eb5ce9e8f440f8973c5686aae112807
capaci <rafaelcp123@gmail.com>
2016-07-02 20:32:02 -0300

    page scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $( document ).ready(function(){
     /* sidebar mobile */
     $(".button-collapse").sideNav();
-})
+
+    $('.page-scroll a').click(function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
