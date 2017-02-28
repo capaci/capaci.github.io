135d3cf59f380157c94357d35acedf6f9074d6d7
capaci <rafaelcp123@gmail.com>
2017-02-28 11:17:07 -0300

    acessibilidade: abre menu quando navega com o teclado

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    var menuLinks = $('.sub-menu__item--link');
+    var firstMenuLink = menuLinks[0];
+    var lastMenuLink = menuLinks[menuLinks.length - 1];
+
 	$(document).delegate('.open', 'click', function(event){
 		$(this).addClass('oppenned');
 		event.stopPropagation();
@@ -10,4 +14,18 @@ $(document).ready(function() {
 		$('.open').removeClass('oppenned');
 		event.stopPropagation();
 	});
+
+    /* show menu when user focused on a menu link using the keyboard */
+    $(document).delegate('.sub-menu__item--link', 'focus', function(event){
+		$('.open').addClass('oppenned');
+		event.stopPropagation();
+	})
+
+    /* hide menu when user get out of menu links using the keyboard */
+    $(document).delegate('.sub-menu__item--link', 'focusout', function(event){
+        if (this === lastMenuLink || this === firstMenuLink) {
+            $('.open').removeClass('oppenned');
+        }
+		event.stopPropagation();
+	})
 });
